c65452b100ef371695f6b47ac1d3e4d5db40f8db
Diego Garces <diego.garces.sierra@udla.edu.ec>
2026-05-12 11:40:21 -0500

    primer commit segundo ejercicio

---
+++ Ejercicio2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    char vocales[] = {'a', 'e', 'i', 'o', 'u'};
+    
+
+    
+    for (int i = 0; i < 5; i++) {
+        printf("vocales[%d] = %c\t", i, vocales[i]);
+        
+    }
+    return 0;
+}
